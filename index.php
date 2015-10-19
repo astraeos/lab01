daccf68cb509af6c92b7f61066cd78de29297210
kieranpark <kieran_afc_2k6@hotmail.com>
2015-10-19 15:08:12 +0100

    2.1

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
     <body>
         <p>
             <?php
-                echo "Hello World";
+                echo "Hello, world";
+                echo "Hello, " . " " . "world" . "!";
+                echo 5 * 7;
+                $myname = "Frodo Baggins";
+                $myage = 111;
+                echo "My name is " . $myname . "and I am " . $myage;
+                //IN-LINE COMMENT
+                /*  BLOCK
+                    LINE
+                    COMMENT
+                */
             ?>
         </p>
     </body>
